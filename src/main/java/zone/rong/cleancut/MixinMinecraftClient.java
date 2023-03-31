f2ed3771a4febc1c3bd69d65aefb6cedec194e5a
unilock <unilock@fennet.rentals>
2023-03-31 10:05:50 -0400

    Update to 1.19.4
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/zone/rong/cleancut/MixinMinecraftClient.java
+++ src/main/java/zone/rong/cleancut/MixinMinecraftClient.java
@@ -6,33 +6,30 @@ import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.projectile.ProjectileUtil;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(MinecraftClient.class)
 public class MixinMinecraftClient {
 
     @Shadow @Nullable public ClientWorld world;
     @Shadow @Nullable public ClientPlayerEntity player;
+    @Shadow @Nullable public HitResult crosshairTarget;
 
-    @Shadow @Nullable public ClientPlayerInteractionManager interactionManager;
-
-    @Inject(method = "doAttack", at = @At(value = "INVOKE",
-            target = "net/minecraft/client/network/ClientPlayerInteractionManager.attackBlock(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction;)Z"),
-            locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
-    private void beforeStartingToBreakBlock(CallbackInfoReturnable<Boolean> cir, ItemStack itemStack, boolean bl, BlockHitResult blockHitResult, BlockPos pos) {
+    @Redirect(method = "doAttack", at = @At(value = "INVOKE",
+            target = "net/minecraft/client/network/ClientPlayerInteractionManager.attackBlock(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction;)Z"))
+    private boolean beforeStartingToBreakBlock(ClientPlayerInteractionManager interactionManager, BlockPos pos, Direction direction) {
         BlockState state = world.getBlockState(pos);
         if (state.getCollisionShape(this.world, pos).isEmpty() || state.getHardness(this.world, pos) == 0.0F) {
             float reach = interactionManager.getReachDistance();
@@ -41,12 +38,13 @@ public class MixinMinecraftClient {
             Vec3d end = camera.add(rotation.x * reach, rotation.y * reach, rotation.z * reach);
             EntityHitResult result = ProjectileUtil.raycast(player, camera, end, new Box(camera, end), e -> !e.isSpectator() && e.isAttackable(), reach * reach);
             if (result != null) {
-                this.interactionManager.attackEntity(player, result.getEntity());
-                cir.setReturnValue(false);
+                interactionManager.attackEntity(player, result.getEntity());
                 this.player.swingHand(Hand.MAIN_HAND);
+                return false;
             }
         }
-
+        BlockHitResult blockHitResult = (BlockHitResult)this.crosshairTarget;
+        return interactionManager.attackBlock(pos, blockHitResult.getSide());
     }
 
 }
